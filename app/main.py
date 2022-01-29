1be1402b495977c27dbd8696d90b14937f2ddbae
atusi <atu729@icloud.com>
2022-01-30 00:06:31 +0900

    add validation function

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
+import json
 import pathlib
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from cassandra.cqlengine.management import sync_table
+from pydantic.error_wrappers import ValidationError
 
-from app import config, db
+from app import config, db, utils
 from app.users.models import User
+from app.users.schemas import UserSignupSchema, UserLoginSchema
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent  # app/
 TEMPLATE_DIR = BASE_DIR / "templates"
@@ -53,8 +56,16 @@ def login_post_view(request: Request,
                     email: str = Form(...),
                     password: str = Form(...)):
     print(email, password)
+    raw_data = {
+        "email": email,
+        "password": password,
+    }
+    data, errors = utils.valid_schema_data_or_error(raw_data, UserLoginSchema)
+    print(data)
     return templates.TemplateResponse("auth/login.html", {
         "request": request,
+        "data": data,
+        "errors": errors,
     })
 
 
@@ -76,9 +87,16 @@ def signup_post_view(request: Request,
                      password: str = Form(...),
                      password_confirm: str = Form(...)
                      ):
-    print(email, password)
+    raw_data = {
+        "email": email,
+        "password": password,
+        "password_confirm": password_confirm
+    }
+    data, errors = utils.valid_schema_data_or_error(raw_data, UserSignupSchema)
     return templates.TemplateResponse("auth/signup.html", {
         "request": request,
+        "data" : data,
+        "errors": errors,
     })
 
 
