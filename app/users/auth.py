657fe14d3556110ce5abb4d51abb3a89f0600ded
atusi <atu729@icloud.com>
2022-01-30 17:55:38 +0900

    implemented authentication method

---
+++ app/users/auth.py
@@ -0,0 +1,44 @@
+import datetime
+from jose import jwt, ExpiredSignatureError
+from app import conf
+
+from app.models import User
+
+settings = conf.get_settings()
+
+
+def authenticate(email, password):
+    # 1st step
+    try:
+        user_obj = User.objects.get(email=email)  # if user does not exist throw exception
+    except Exception as e:
+        user_obj = None
+    if not user_obj.verify_password(password):  # if users password is not correct throw return None
+        return None
+    return user_obj
+
+
+def login(user_obj, expires=5):  # create token if user passed 1st step
+    # 2nd step
+    raw_data = {
+        "user_id": f"{user_obj.id}",
+        "role": "admin",
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=expires)
+    }
+    return jwt.encode(raw_data, settings.secret_key, algorithm=settings.jwt_algorithm)
+
+
+def verify_user_id(token):
+    # 3rd step
+    data = {}
+    verified = False
+    try:
+        data = jwt.decode(token, settings.secret_key, algorithms=[settings.jwt_algorithm])
+        verified = True
+    except ExpiredSignatureError as e:
+        print(e)
+    except:
+        pass
+    if 'user_id' not in data:
+        return None
+    return data, verified
