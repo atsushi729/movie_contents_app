5abaf59978c85050a339bc83d7ef6a52ad1cc8e5
atusi <atu729@icloud.com>
2022-02-13 18:01:23 +0900

    tested playlist function

--- app/playlists/models.py
+++ app/playlists/models.py
@@ -5,6 +5,7 @@ from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
 from app import config
+from app.videos.models import Video
 
 settings = config.get_settings()
 
@@ -14,5 +15,27 @@ class Playlist(Model):
     db_id = columns.UUID(primary_key=True, default=uuid.uuid1)
     user_id = columns.UUID()
     updated = columns.DateTime(default=datetime.utcnow())
-    host_ids = columns.List(value_type=columns.Test)
+    host_ids = columns.List(value_type=columns.Text)
     title = columns.Text()
+
+    def add_host_ids(self, host_ids=[], replace_all=False):
+        if not isinstance(host_ids, list):
+            return False
+        if replace_all:
+            self.host_ids = host_ids
+        else:
+            self.host_ids += host_ids
+        self.updated = datetime.utcnow()
+        self.save()
+        return True
+
+    def get_videos(self):
+        videos = []
+        for host_id in self.host_ids:
+            try:
+                video_obj = Video.objects.get(host_id=host_id)
+            except:
+                video_obj = None
+            if video_obj is not None:
+                videos.append(video_obj)
+        return videos
