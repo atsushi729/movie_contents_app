0dabe71f7de91a349d5c2a7ac3d0b3faa7abd32e
atusi <atu729@icloud.com>
2022-02-22 09:03:11 +0900

    added video edit or remove function

--- app/videos/routers.py
+++ app/videos/routers.py
@@ -9,7 +9,7 @@ from app.shortcuts import render, redirect, get_object_or_404, is_htmx
 from app.watch_events.models import WatchEvent
 
 from .models import Video
-from .schemas import VideoCreateSchema
+from .schemas import VideoCreateSchema, VideoEditSchema
 
 router = APIRouter(
     prefix='/videos'
@@ -74,7 +74,7 @@ def video_list_view(request: Request):
 
 
 @router.get("/{host_id}", response_class=HTMLResponse)
-def video_list_view(request: Request, host_id: str):
+def video_detail_view(request: Request, host_id: str):
     obj = get_object_or_404(Video, host_id=host_id)
     if request.user.is_authenticated:
         user_id = request.user.username
@@ -85,3 +85,38 @@ def video_list_view(request: Request, host_id: str):
         "object": obj,
     }
     return render(request, "videos/detail.html", context)
+
+
+@router.get("/{host_id}/edit", response_class=HTMLResponse)
+@login_required
+def video_edit_view(request: Request, host_id: str):
+    obj = get_object_or_404(Video, host_id=host_id)
+    context = {
+        "object": obj,
+    }
+    return render(request, "videos/edit.html", context)
+
+
+@router.post("/{host_id}/edit", response_class=HTMLResponse)
+@login_required
+def video_edit_post_view(
+        request: Request,
+        host_id: str,
+        is_htmx=Depends(is_htmx),
+        title: str = Form(...), 
+        url: str = Form(...)):
+    raw_data = {
+        "title": title,
+        "url": url,
+        "user_id": request.user.username
+    }
+    obj = get_object_or_404(Video, host_id=host_id)
+    context = {
+        "object": obj,
+    }
+    data, errors = utils.valid_schema_data_or_error(raw_data, VideoEditSchema)
+    if len(errors) > 0:
+        return render(request, "videos/edit.html", context, status_code=400)
+    obj.title = data.get('title') or obj.title
+    obj.update_video_url(url, save=True)
+    return render(request, "videos/edit.html", context, status_code=400)
