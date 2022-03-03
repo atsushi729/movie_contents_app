41fd3ce1b784d0d77e3259e7639ef82b765477dd
atusi <atu729@icloud.com>
2022-03-04 08:50:40 +0900

    updated search index

--- app/indexing/client.py
+++ app/indexing/client.py
@@ -2,6 +2,14 @@ from algoliasearch.search_client import SearchClient
 
 from app import config
 
+from app.playlists.models import Playlist
+from app.videos.models import Video
+
+from .schemas import (
+    PlaylistIndexSchema,
+    VideoIndexSchema,
+)
+
 settings = config.get_settings()
 ALGOLIA_APP_ID = settings.algolia_app_id
 ALGOLIA_API_KEY = settings.algolia_api_key
@@ -9,15 +17,32 @@ ALGOLIA_INDEX_NAME = settings.algolia_index_name
 
 
 def get_index(name=ALGOLIA_INDEX_NAME):
-    client = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY, ALGOLIA_INDEX_NAME)
+    client = SearchClient.create(
+        ALGOLIA_APP_ID,
+        ALGOLIA_API_KEY
+    )
     index = client.init_index(name)
     return index
 
 
+def get_dataset():
+    playlist_q = [dict(x) for x in Playlist.objects.all()]
+    playlists_dataset = [PlaylistIndexSchema(**x).dict() for x in playlist_q]
+    video_q = [dict(x) for x in Video.objects.all()]
+    videos_dataset = [VideoIndexSchema(**x).dict() for x in video_q]
+    dataset = playlists_dataset + videos_dataset
+    return dataset
+
+
 def update_index():
     index = get_index()
-    pass
-    return 
+    dataset = get_dataset()
+    idx_resp = index.save_objects(dataset).wait()
+    try:
+        count = len(list(idx_resp)[0]['objectIDs'])
+    except:
+        count = None
+    return count
 
 
 def search_index(query):
